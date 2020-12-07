d32f4057142bfb11000e4b76871bf42342d1265c
WillDotWhite <willdotwhiteis@gmail.com>
2020-12-07 16:54:04 +0000

    Version reimport and workspace changes

--- Assets/_Project/Dialogue/DialogueLine.cs
+++ Assets/_Project/Dialogue/DialogueLine.cs
@@ -4,6 +4,7 @@
     /// A DialogueLine is a single entry in a DialogueRecord (which is a subset of a larger conversation/narration)
     /// </summary>
     [System.Serializable]
+    // TODO abstract and break out
     public class DialogueLine {
         // TODO: Comment and markup after readding "meta"
         public string speaker;
